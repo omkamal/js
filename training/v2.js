6f92abadc05e750673a3303b0ee35504262e1dec
Omar Kamal Hosney <omkamal@gmail.com>
2021-01-01 19:58:56 -0800

    Moving basic training to director

---
+++ training/v2.js
@@ -0,0 +1,33 @@
+var x = 'test';
+
+Number.isNaN(x);
+Number.isNaN(x/2);
+
+var yesterday = new Date("Jan 6, 2020");
+yesterday.toUTCString();
+
+typeof yesterday;
+
+transcript = {'gpa':3.4};
+
+var y = transcript.gpa;
+
+var z = String(y);
+z;
+
+// Type coercion
+console.log(x + ' '+ transcript.gpa + ' '); 
+
+Number('3.5');
+
+Boolean(null);
+
+Boolean('sds');
+Boolean(4.3);
+
+!!'sds';
+
+3.0 == 3;
+
+2.0 === '2';
+2.0 == '2';
