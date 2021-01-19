1a0320a970abb73ad24db5f8512a94b01171cc6c
omar hosney <omkamal@gmail.com>
2021-01-19 09:17:56 -0800

    Sample Js

--- node/training/p1/sample.js
+++ node/training/p1/sample.js
@@ -1,1 +1,19 @@
+
 require('fs')
+require('process')
+const Table = require('cli-table');
+
+console.log(process.argv)
+
+const t = new Table({
+    head: ['Command','Description'],
+    colWidths: [5,50]
+    });
+
+t.push(['ls','list the files in a directory'],
+        ['rm','deletes a file in a directory']
+);
+
+console.log(t.toString());
+
+module.exports = {};
